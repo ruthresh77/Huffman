dc2534781c92b73c33252173ffe92b388aa83fa6
ruthresh77 <41472753+ruthresh77@users.noreply.github.com>
2021-06-26 09:08:13 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,82 @@
-# Test functions goes here
 import unittest
 
 from huffman import encode, decode
 
 class TestHuffman(unittest.TestCase):
-	# write all your tests here
-	# function name should be prefixed with 'test'
+    	def test_1_encode1(self):
 
-	def test_encode(self):
-		encode("", "")
-		assert True
+		f_in = 'test_encode1.txt'
+		f_out = 'test_encode_res1.txt'
 
-	def test_decode(self):
-		decode("", "")
-		assert True
+		f_encode = open(f_in,'w+')
+
+		# test-1
+		inp_content = 'abcde'
+		actual_result = '110111000110'
+
+		f_encode.write(inp_content)
+		f_encode.close()
+
+		encode(f_in, f_out)
+		f_out = open(f_out,'r')
+		output = f_out.readlines()
+
+		check = (output[0]==actual_result)
+
+		self.assertTrue(check)
+
+	def test_2_decode1(self):
+
+		f_in = 'test_encode_res1.txt'
+		f_out = 'test_decode_res1.txt'
+
+		decode(f_in, f_out)
+
+		f_decode = open(f_out,'r')
+		output = f_decode.readlines()
+
+		actual_result = 'abcde'
+		check = (output[0]==actual_result)
+
+		self.assertTrue(check)
+	def test_3_encode2(self):
+
+		f_in = 'test_encode2.txt'
+		f_out = 'test_encode_res2.txt'
+
+		f_encode = open(f_in,'w+')
+
+		# test-1
+		inp_content = 'LJKJI|}|{|{>?'
+		actual_result = '1100100110110011100111110110101101000001'
+
+		f_encode.write(inp_content)
+		f_encode.close()
+
+		encode(f_in, f_out)
+		f_out = open(f_out,'r')
+		output = f_out.readlines()
+
+		check = (output[0]==actual_result)
+
+		self.assertTrue(check)
+
+
+	def test_4_decode2(self):
+
+		f_in = 'test_encode_res2.txt'
+		f_out = 'test_decode_res2.txt'
+
+		decode(f_in, f_out)
+
+		f_decode = open(f_out,'r')
+		output = f_decode.readlines()
+
+		actual_result = 'LJKJI|}|{|{>?'
+		check = (output[0]==actual_result)
+
+		self.assertTrue(check)             
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
